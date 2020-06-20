d03b69492b70e2e3bf23f12d4d25e6f6d3185ed4
Elias Yuan <a@jthv.ai>
2020-06-21 02:56:45 +1000

    Add 1370A

---
+++ 1370/A.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: X11
+// 2020-06-21 (In Contest)
+
+#include <stdio.h>
+
+int main(void) {
+  int t;
+  scanf("%d", &t);
+
+  long curr;
+  for(int i = 0; i < t; i++) {
+    scanf("%ld", &curr);
+    printf("%ld\n", curr / 2);
+  }
+
+  return 0;
+}
