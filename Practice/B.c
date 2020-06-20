e2fb7acff69daf0236897ecf0ef217c192ee4736
Elias Yuan <a@jthv.ai>
2020-06-20 21:10:59 +1000

    Update Practice/B
    Changed algorithm from quicksort to mergesort

--- Practice/B.c
+++ Practice/B.c
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int strqsort(char s[], int lo, int hi, int qu);
+void mergesort(char s[], int lo, int hi);
+void merge(char s[], int lo, int mi, int hi);
 
 int main(void) {
   int N,Q;
@@ -14,80 +16,65 @@ int main(void) {
   for (int i = 0; i < N; i++)
     ans[i] = 'A' + i;
 
-  if (strqsort(ans, 0, N - 1, Q)) {
-    free(ans);
-    return 1;
-  }
+  if (N > 5)
+    mergesort(ans, 0, N - 1);
+  else
+    return 1; // !DO Hardcode something for case 3
 
   printf("! %s\n", ans);
   free(ans);
-
   return 0;
 }
 
-int strqsort(char a[], int lo, int hi, int qu) {
-  if (lo >= hi)
-    return 0;
+void mergesort(char s[], int lo, int hi) {
+  if (lo < hi) {
+    int mi = lo + (hi - lo) / 2;
 
-  int h, l, p, t;
+    mergesort(s, lo, mi);
+    mergesort(s, mi + 1, hi);
+
+    merge(s, lo, mi, hi);
+  }
+}
+
+void merge(char s[], int lo, int mi, int hi) {
+  int hl = mi - lo + 1,
+      hr = hi - mi;
+  char l[hl],
+       r[hr];
+  strncpy(l, s + lo, hl * sizeof(char));
+  strncpy(r, s + mi + 1, hr * sizeof(char));
+
+  int i = 0,
+      j = 0,
+      k = lo;
   char reply;
 
-  l = lo;
-  h = hi;
-  p = a[hi];
+  while (i < hl && j < hr) {
+    printf("? %c %c\n", l[i], r[j]);
+    fflush(stdout);
+    scanf(" %c", &reply);
 
-  do {
-    while (l < h) {
-      if (a[l] == p) {
-        l++;
-        continue;
-      }
-
-      if (!qu)
-        return 1;
-
-      printf("? %c %c\n", a[l], p);
-      fflush(stdout);
-      scanf(" %c", &reply);
-      qu--;
-
-      if (reply == '>')
-        break;
-      else
-        l++;
+    if (reply == '<') {
+      s[k] = l[i];
+      i++;
     }
-
-    while (h > l) {
-      if (a[h] == p) {
-        h--;
-        continue;
-      }
-
-      if (!qu)
-        return 1;
-
-      printf("? %c %c\n", a[h], p);
-      fflush(stdout);
-      scanf(" %c", &reply);
-      qu--;
-
-      if (reply == '<')
-        break;
-      else
-        h--;
+    else {
+      s[k] = r[j];
+      j++;
     }
+  k++;
+  }
 
-    if (l < h) {
-      t = a[l];
-      a[l] = a[h];
-      a[h] = t;
-    }
-  } while (l < h);
+  while (i < hl) {
+    s[k] = l[i];
+    i++;
+    k++;
+  }
 
-  a[hi] = a[l];
-  a[l] = p;
-
-  if (strqsort(a, lo, l - 1, qu))
-    return 1;
-  return strqsort(a, l + 1, hi, qu);
+  while (j < hr) {
+    s[k] = r[j];
+    j++;
+    k++;
+  }
 }
